616d57492f3b0ae02be51048713fef29f348bfed
emtupp <emilyc.tupper@gmail.com>
2020-12-10 20:46:18 -0500

    add axios call to api

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";  
 
 import "components/Application.scss";
 import Appointment from "components/Appointment";
@@ -52,27 +53,18 @@ const appointments = [
   }
 ];
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
-
 export default function Application(props) {
 
   const [day, setDay] = useState('Monday');
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    axios.get(`http://localhost:8001/api/days`)
+    .then(response => {
+      console.log(response.data)
+      setDays([...response.data])
+    });
+  }, []);
 
   return (
     <main className="layout">
